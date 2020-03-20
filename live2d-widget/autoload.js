c86f489a7b4fb3483ad77d95e0b80a8a277e98be
gkm0120 <gkm0120@163.com>
2020-03-20 16:01:40 +0800

    Site updated: 2020-03-20 16:01:40

---
+++ live2d-widget/autoload.js
@@ -0,0 +1,3 @@
+// build time:Tue Mar 17 2020 15:13:22 GMT+0800 (GMT+08:00)
+const live2d_path="D:\\blog\\gkmBlog\\themes\\hexo-theme-matery-1.0.1\\source\\live2d-widget";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
+//rebuild by ne
