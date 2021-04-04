ac218c725d5a9703742248375e15969b04809c1a
gkm0120 <ineukesscg@gmail.com>
2021-04-04 14:51:40 +0800

    Site updated: 2021-04-04 14:51:39

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,23 +1,18 @@
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) {
-    /* code */
-    var initCopyCode = function(){
-      var copyHtml = '';
-      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-      copyHtml += '</button>';
-      $(".highlight").wrap($('<div class="highlight-wrap"></div>'));
-      $('.highlight-wrap').prepend(copyHtml);
-      var clipboard = new ClipboardJS('.btn-copy', {
-          target: function(trigger) {
-              return trigger.nextElementSibling;
-          }
-      });
-      // 成功提示
-      clipboard.on('success', function(e) {
-        toastPlug('复制成功!', 2000)
-      });
-    }
-    initCopyCode();
-  }(window, document);
-  
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    //fa fa-globe可以去字体库替换自己想要的图标
+    copyHtml += '  <i class="fa fa-clipboard"></i><span>复制</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+      target: function(trigger) {
+        return trigger.nextElementSibling;
+      }
+    });
+  }
+  initCopyCode();
+}(window, document);
